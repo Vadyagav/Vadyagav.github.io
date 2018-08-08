d12d8c631243b81a2e6bb21d550f34517d1c8249
Havrylov <vadyagav@gmail.com>
2018-08-09 00:21:21 +0300

    update prompt task

--- js-react-school-basics/homework/01-prompt-write/script.js
+++ js-react-school-basics/homework/01-prompt-write/script.js
@@ -3,5 +3,5 @@ let birthday = prompt('What is your birthday');
 let city = prompt('What is your city');
 
 document.getElementById('name').innerText = name;
-document.getElementById('birthday').innerText = birthday;
-document.getElementById('city').innerText = city;
+document.getElementById('birthday').innerText = `Дата рождения: ${birthday}`;
+document.getElementById('city').innerText = `Место жительства: ${city}`;
